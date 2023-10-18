a8a93046302a518e9b8875cbe60e119641f261b1
alanmoex <alan.moex@gmail.com>
2023-10-18 16:55:04 -0300

    se agrego opciones en el menu principal

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from usuario import Usuario
+from estudiante import Estudiante
+from profesor import Profesor
 
-lista_estudiantes= []
-lista_profesores= []
+lista_estudiantes= [Estudiante("alan", "moex", "alan@gmail.com", "alanmoex", "123", 2023)]
+lista_profesores= [Profesor("Juan", "Perez", "juanperez@gmail.com", "juanperez", "licenciado", 2000)]
 
 def menu_principal():
     print("1 - Ingresar cómo usuario")
@@ -21,11 +22,25 @@ while True:
     menu_principal()
     op = int(input("Ingrese una opción\n"))
     if op == 1:
+        encontrado = False
         email = str(input("Ingrese su email:\n"))
         contraseña = str(input("Ingrese su contraseña:\n"))
         for estudiante in lista_estudiantes:
             if estudiante.email == email:
-                pass
+                encontrado = True
+                validado = estudiante.validar_credenciales(email, contraseña)
+                if validado:
+                    print("\nHa ingresado correctamente\n")
+                    while True:
+                        menu_estudiante()
+                        opEst = int(input("Ingrese una opción\n"))
+                        if opEst==1:
+                            pass
+                        elif opEst == 2:
+                            pass
+                        elif opEst == 3:
+                            break
+
     elif  op == 2:
         pass
     elif op == 3:
