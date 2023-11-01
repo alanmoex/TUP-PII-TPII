fb57610e081fb34e4f2077ac1090984ef4195a7d
alanmoex <alan.moex@gmail.com>
2023-10-31 22:12:58 -0300

    se creo el metodo de clase y propiedad codigo
    se creo la propiedad codigo, la variable prox_nro y el metodo de clase que autoincrementa el codigo. tambien se creo el property de codigo

--- curso.py
+++ curso.py
@@ -3,9 +3,11 @@ import string
 
 class Curso:
     largo_contraseña_matriculacion = 6
+    prox_nro = 0
 
     def __init__(self, nombre: str) -> None:
         self.__nombre = nombre
+        self.__codigo = Curso.get_prox_nro()
         self.__contraseña_matriculacion = self.generar_contraseña()
 
     @property
@@ -17,6 +19,10 @@ class Curso:
         self.__nombre = nombre
 
     @property
+    def codigo(self) -> int:
+        return self.__codigo
+
+    @property
     def contraseña_matriculacion(self) -> str:
         return self.__contraseña_matriculacion
 
@@ -26,3 +32,8 @@ class Curso:
     @classmethod
     def generar_contraseña(cls) -> str:
         return "".join(random.choice(string.ascii_letters + string.digits) for _ in range(cls.largo_contraseña_matriculacion))
+
+    @classmethod
+    def get_prox_nro(cls) -> int:
+        cls.prox_nro += 1
+        return cls.prox_nro
