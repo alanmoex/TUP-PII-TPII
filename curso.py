a64b14fde1220ed44d9a6e42252dcf6bb97724b5
alanmoex <alan.moex@gmail.com>
2023-10-31 22:14:28 -0300

    se agrego la propiedad carrera
    ahora la clase curso recibe como parametro una carrera tambien. se agrego el property y el setter para esta

--- curso.py
+++ curso.py
@@ -5,10 +5,11 @@ class Curso:
     largo_contraseña_matriculacion = 6
     prox_nro = 0
 
-    def __init__(self, nombre: str) -> None:
+    def __init__(self,carrera:object, nombre: str) -> None:
         self.__nombre = nombre
         self.__codigo = Curso.get_prox_nro()
         self.__contraseña_matriculacion = self.generar_contraseña()
+        self.__carrera = carrera
 
     @property
     def nombre(self) -> str:
@@ -25,6 +26,12 @@ class Curso:
     @property
     def contraseña_matriculacion(self) -> str:
         return self.__contraseña_matriculacion
+    @property
+    def carrera(self) -> object:
+        return self.__carrera
+    @carrera.setter
+    def carrera(self, carrera: object):
+        self.__carrera = carrera
 
     def __str__(self) -> str:
         return f"Materia: {self.__nombre}\nContraseña: {self.__contraseña_matriculacion}"
